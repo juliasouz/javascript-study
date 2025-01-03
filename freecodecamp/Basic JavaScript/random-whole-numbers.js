d1edd1283fa90e8cac651fa59cbd544f62fa27c0
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 15:53:26 -0300

    Exercise: Implement randomWholeNum function to generate random whole numbers from 0 to 9

---
+++ freecodecamp/Basic JavaScript/random-whole-numbers.js
@@ -0,0 +1,6 @@
+/* Use this technique (example: Math.floor(Math.random() * 20);)
+to generate and return a random whole number in the range from 0 to 9. */
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
