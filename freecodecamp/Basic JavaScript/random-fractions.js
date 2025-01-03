21dd06f8f661cd627fe0a660d8b43a5050d88344
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 15:48:31 -0300

    Exercise: Implement randomFraction function to generate random numbers

---
+++ freecodecamp/Basic JavaScript/random-fractions.js
@@ -0,0 +1,6 @@
+/* Generate Random Fractions with JavaScript
+Change randomFraction to return a random number instead of returning 0. */
+
+function randomFraction() {  
+    return Math.random();  
+}
