2b09e295209428570dad828e1545c83f26417681
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 15:12:13 -0300

    Exercise: Add for loop example to push odd numbers from 9 to 1 into an array

---
+++ freecodecamp/Basic JavaScript/backwards-for.js
@@ -0,0 +1,7 @@
+/* Count Backwards With a For Loop
+Push the odd numbers from 9 through 1 to myArray using a for loop. */
+
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
