21da0523828776d182bcc39d0f2f35ab2c2423ed
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 15:39:38 -0300

    Exercise: Add recursive function to calculate the sum of the first n elements in an array

---
+++ freecodecamp/Basic JavaScript/recursion.js
@@ -0,0 +1,10 @@
+/* Replace Loops using Recursion
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr. */
+
+function sum(arr, n) {
+    if (n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+}
