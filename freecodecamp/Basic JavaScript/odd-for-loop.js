5e568259b908fba7c729ed7c592184dca01b0fef
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 15:07:31 -0300

    Exercise: Add for loop example to iterate odd numbers from 1 to 9

---
+++ freecodecamp/Basic JavaScript/odd-for-loop.js
@@ -0,0 +1,8 @@
+/* Iterate Odd Numbers With a For Loop
+Push the odd numbers from 1 through 9 to myArray using a for loop. */
+
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
