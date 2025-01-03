33f219be4f940d6116e893724a9e2627f46ded15
Julia Souza <julia-souza@uergs.edu.br>
2025-01-02 23:46:44 -0300

    Exercise: Add new album to myMusic array with artist, title, release year, and formats

---
+++ freecodecamp/Basic JavaScript/complex-objects.js
@@ -0,0 +1,28 @@
+/* Manipulating Complex Objects
+Add a new album to the myMusic array.
+Add artist and title strings, release_year number,and a formats array of strings. */
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Imagine Dragons",
+      "title": "Mercury Act 1",
+      "release_year": 2021,
+      "formats": [
+        "CD",
+        "Vinyl",
+        "Cassette",
+        "LP"
+      ]
+    }
+  ];
