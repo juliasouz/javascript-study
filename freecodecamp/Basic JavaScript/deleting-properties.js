5963119b164b7f8ffece32d759ead150613872e7
Julia Souza <julia-souza@uergs.edu.br>
2025-01-02 23:09:01 -0300

    Exercise: Add example of deleting properties from a JavaScript object

---
+++ freecodecamp/Basic JavaScript/deleting-properties.js
@@ -0,0 +1,12 @@
+/* Delete Properties from a JavaScript Object
+Delete the tails property from myDog. You may use either dot or bracket notation. */
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  delete myDog.tails;
