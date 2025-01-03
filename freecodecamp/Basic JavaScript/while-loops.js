f55e5e4a8b7354bc9e14a0fbac662482f791de96
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 00:10:24 -0300

    Exercise: Add while loop example to populate an array with descending numbers

---
+++ freecodecamp/Basic JavaScript/while-loops.js
@@ -0,0 +1,9 @@
+/* Iterate with JavaScript While Loops
+Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop. */
+
+const myArray = [];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
