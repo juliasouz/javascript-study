1ff6c0921729ac5ec828157cf7f78a22a38690e5
Julia Souza <julia-souza@uergs.edu.br>
2025-01-02 22:52:14 -0300

    Exercise: Add example of accessing object properties using dot notation

---
+++ freecodecamp/Basic JavaScript/object-properties.js
@@ -0,0 +1,13 @@
+/* Accessing Object Properties with Dot Notation
+Read in the property values of testObj using dot notation.
+Set the variable hatValue equal to the object's property hat
+and set the variable shirtValue equal to the object's property shirt. */
+
+const testObj = {
+    hat: "ballcap",
+    shirt: "jersey",
+    shoes: "cleats"
+  };
+  
+  const hatValue = testObj.hat;
+  const shirtValue = testObj.shirt;
