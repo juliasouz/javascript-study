8c36fe72a9e750121d782ef972728012e9b6b0b6
Julia Souza <julia-souza@uergs.edu.br>
2025-01-03 16:03:04 -0300

    Exercise: Implement convertToInteger function using parseInt to convert string to integer

---
+++ freecodecamp/Basic JavaScript/parseInt-function.js
@@ -0,0 +1,9 @@
+/* Use the parseInt Function
+Use parseInt() in the convertToInteger function so it converts the input string str into an integer,
+and returns it. */
+
+function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+convertToInteger("56");
