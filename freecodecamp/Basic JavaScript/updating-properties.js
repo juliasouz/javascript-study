0ec2698f1b73e04551367a968a155e9a8f0e9782
Julia Souza <julia-souza@uergs.edu.br>
2025-01-02 23:07:11 -0300

    Exercise: Add examples for adding and updating properties in JavaScript objects

---
+++ freecodecamp/Basic JavaScript/updating-properties.js
@@ -0,0 +1,12 @@
+/* Updating Object Properties
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder.
+You can use either dot or bracket notation. */
+
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog["name"] = "Happy Coder";
