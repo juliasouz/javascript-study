f24a409b738d2324054e6a8c726a8bc3f96b2317
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 17:17:29 -0300

    Exercise: Refactoring setGear function in bicycle object to use shorthand syntax

---
+++ freecodecamp/ES6/declarativeFunctions.js
@@ -0,0 +1,11 @@
+/* Write Concise Declarative Functions with ES6
+Refactor the function setGear inside the object bicycle to use the shorthand syntax described above. */
+
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
