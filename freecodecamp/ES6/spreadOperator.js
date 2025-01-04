405a364003ba903caa3ea89fbca3cb6ca4f088f3
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 00:27:49 -0300

    Exercise: Implement spread operator to copy array contents

---
+++ freecodecamp/ES6/spreadOperator.js
@@ -0,0 +1,9 @@
+/* Use the Spread Operator to Evaluate Arrays In-Place
+Copy all contents of arr1 into another array arr2 using the spread operator. */
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];
+
+console.log(arr2);
