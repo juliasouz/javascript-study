8a07f85c1c8346453dfae8dec157b80ebe3a4110
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 21:46:11 -0300

    Exercise: Adding regex to match characters that occur one or more times

---
+++ freecodecamp/Regular Expressions/matchCharacters.js
@@ -0,0 +1,7 @@
+/* Match Characters that Occur One or More Times
+You want to find matches when the letter s occurs one or more times in Mississippi.
+Write a regex that uses the + sign. */
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/gi;
+let result = difficultSpelling.match(myRegex);
