990aaa364dcc4fdebc2bba3cf5e2b040120feea7
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 21:25:21 -0300

    Exercise: Adding functionality to extract multiple matches using regex

---
+++ freecodecamp/Regular Expressions/multipleExtractMatches.js
@@ -0,0 +1,7 @@
+/* Find More Than the First Match
+Using the regex starRegex, find and extract both Twinkle words from the string twinkleStar.
+Note: You can have multiple flags on your regex like /search/gi */
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi;
+let result = twinkleStar.match(starRegex);
