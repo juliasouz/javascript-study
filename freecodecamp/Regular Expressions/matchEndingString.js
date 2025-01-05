fb760ffc2cbf32ca95293b8a2958e28f91246ea0
Julia Souza <julia-souza@uergs.edu.br>
2025-01-05 15:32:56 -0300

    Exercise: Adding regex to match ending string patterns

---
+++ freecodecamp/Regular Expressions/matchEndingString.js
@@ -0,0 +1,6 @@
+/* Match Ending String Patterns
+Use the anchor character ($) to match the string caboose at the end of the string caboose. */
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/;
+let result = lastRegex.test(caboose);
