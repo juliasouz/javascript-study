adb694bbc90a8dc33ee304c52da02361b7822938
Julia Souza <julia-souza@uergs.edu.br>
2025-01-05 19:58:29 -0300

    Exercise: Adding regex to match the word "Timber" with exactly four 'm's

---
+++ freecodecamp/Regular Expressions/specifyExactNumbersOfMatches.js
@@ -0,0 +1,6 @@
+/* Specify Exact Number of Matches
+Change the regex timRegex to match the word Timber only when it has four letter m's. */
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/;
+let result = timRegex.test(timStr);
