81b24f57d992dcab77a7b036a4160d2cc08dd6b6
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 21:07:10 -0300

    Exercise: Adding regex test method implementation for string matching

---
+++ freecodecamp/Regular Expressions/testMethod.js
@@ -0,0 +1,6 @@
+/* Using the Test Method
+Apply the regex myRegex on the string myString using the .test() method. */
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
