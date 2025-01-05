393985d703848b0c9be68880d62f4fe968e58d13
Julia Souza <julia-souza@uergs.edu.br>
2025-01-04 21:09:12 -0300

    Exercise: Implementing regex to match literal strings for Waldo detection

---
+++ freecodecamp/Regular Expressions/matchLiteralStrings.js
@@ -0,0 +1,6 @@
+/* Match Literal Strings
+Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match. */
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
