17388acead91ae51b60c805d959ad3d0ada902ed
Julia Souza <julia-souza@uergs.edu.br>
2025-01-05 22:48:28 -0300

    Exercise: Adding JavaScript console logging example for variable value checking

---
+++ freecodecamp/Debugging/usingJSConsole.js
@@ -0,0 +1,11 @@
+/* Use the JavaScript Console to Check the Value of a Variable
+Use the console.log() method to print the value of the variable a where noted in the code. */
+
+let a = 5;
+let b = 1;
+a++;
+console.log(a);
+
+
+let sumAB = a + b;
+console.log(sumAB);
