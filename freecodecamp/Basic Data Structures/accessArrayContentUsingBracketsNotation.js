7095bf292054a26df06c99d08eaa8139958111ec
Julia Souza <julia-souza@uergs.edu.br>
2025-01-06 21:35:13 -0300

    Exercise: Adding a script to access and modify array contents using bracket notation

---
+++ freecodecamp/Basic Data Structures/accessArrayContentUsingBracketsNotation.js
@@ -0,0 +1,7 @@
+/* Access an Array's Contents Using Bracket Notation
+In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want,
+besides the letter b. */
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "bariloche";
+console.log(myArray);
