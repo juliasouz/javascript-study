f28b1ba09c98b28d45307a0d2f2b41328b3b83fa
Julia Souza <julia-souza@uergs.edu.br>
2025-01-09 17:17:21 -0300

    Exercise: Defining a constructor function for Dog with properties name, color, and numLegs

---
+++ freecodecamp/Object Oriented Programming (OOP)/defineConstructorFunction.js
@@ -0,0 +1,9 @@
+/* Define a Constructor Function
+Create a constructor, Dog, with properties name, color, and numLegs that are set to a string,
+a string, and a number, respectively. */
+
+function Dog() {
+    this.name = "Luna";
+    this.color = "grey";
+    this.numLegs = 4;
+}
