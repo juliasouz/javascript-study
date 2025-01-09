b473ffe6c4c6ffd94cff901a8de1d4abe94eeba6
Julia Souza <julia-souza@uergs.edu.br>
2025-01-09 00:36:00 -0300

    Exercise: Implementing dot notation to access properties of a dog object

---
+++ freecodecamp/Object Oriented Programming (OOP)/dotNotationToAccessProperties.js
@@ -0,0 +1,10 @@
+/* Use Dot Notation to Access the Properties of an Object
+Print both properties of the dog object to your console. */
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+  
+console.log(dog.name);
+console.log(dog.numLegs);
