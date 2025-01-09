e6705e063e62224ae1232ad621c748649dcc7cb5
Julia Souza <julia-souza@uergs.edu.br>
2025-01-09 00:25:15 -0300

    Exercise: Creating a Basic JavaScript Object for a dog with name and numLegs properties

---
+++ freecodecamp/Object Oriented Programming (OOP)/BasicJavaScriptObject.js
@@ -0,0 +1,7 @@
+/* Create a Basic JavaScript Object
+Create a dog object with name and numLegs properties, and set them to a string and a number, respectively. */
+
+let dog = {
+    name: "Luna",
+    numLegs: 4
+};
