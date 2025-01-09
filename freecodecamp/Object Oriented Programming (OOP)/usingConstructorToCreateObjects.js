c4b1d436f95b1bfc94a37ee608cd8a3938fd2af2
Julia Souza <julia-souza@uergs.edu.br>
2025-01-09 17:20:46 -0300

    Exercise: Creating a Dog constructor and instantiating a new Dog object

---
+++ freecodecamp/Object Oriented Programming (OOP)/usingConstructorToCreateObjects.js
@@ -0,0 +1,10 @@
+/* Use a Constructor to Create Objects
+Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound. */
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+
+let hound = new Dog();
