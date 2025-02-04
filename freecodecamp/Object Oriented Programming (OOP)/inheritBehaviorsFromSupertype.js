da05e9a09ba404acca0471401d7f744c35b6cee3
Julia Souza <julia-souza@uergs.edu.br>
2025-02-03 23:06:53 -0300

    Exercise: Implementing inheritance for Animal using Object.create to create duck and beagle instances

---
+++ freecodecamp/Object Oriented Programming (OOP)/inheritBehaviorsFromSupertype.js
@@ -0,0 +1,14 @@
+/* Inherit Behaviors from a Supertype
+Use Object.create to make two instances of Animal named duck and beagle. */
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
