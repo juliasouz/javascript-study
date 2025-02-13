9a8ff698c843b604a336d08e6462f40ca6e3dec0
Julia Souza <julia-souza@uergs.edu.br>
2025-02-13 00:16:09 -0300

    Exercise: Implement nonMutatingPush function to add elements to an array using concat

---
+++ freecodecamp/Functional Programming/addElementsToTheEndOfAnArrayUsingConcat.js
@@ -0,0 +1,11 @@
+/* Add Elements to the End of an Array Using concat Instead of push
+Change the nonMutatingPush function so it uses concat to merge newItem to the end of original
+without mutating original or newItem arrays. The function should return an array. */
+
+function nonMutatingPush(original, newItem) {
+    return original.concat(newItem);
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
