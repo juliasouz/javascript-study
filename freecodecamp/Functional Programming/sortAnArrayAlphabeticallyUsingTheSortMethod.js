f3f430e3cc6ba631af5861abe1659e67f8ad3cec
Julia Souza <julia-souza@uergs.edu.br>
2025-02-13 00:25:06 -0300

    Exercise: Implement alphabeticalOrder function to sort an array of strings in alphabetical order using the sort method

---
+++ freecodecamp/Functional Programming/sortAnArrayAlphabeticallyUsingTheSortMethod.js
@@ -0,0 +1,9 @@
+/* Sort an Array Alphabetically using the sort Method
+Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+The function should return the sorted array. */
+
+function alphabeticalOrder(arr) {
+    return arr.sort();
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
