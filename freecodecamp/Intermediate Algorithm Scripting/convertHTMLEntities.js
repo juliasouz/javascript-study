301c64a4c6f612cbbb028cdf7d8b52c09a0a32e1
Julia Souza <julia-souza@uergs.edu.br>
2025-02-14 20:33:58 -0300

    Exercise: Implement convertHTML function to convert characters to their corresponding HTML entities

---
+++ freecodecamp/Intermediate Algorithm Scripting/convertHTMLEntities.js
@@ -0,0 +1,13 @@
+/* Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe),
+in a string to their corresponding HTML entities. */
+
+function convertHTML(str) {
+    return str.replace(/&/g, "&amp;")
+              .replace(/</g, "&lt;")
+              .replace(/>/g, "&gt;")
+              .replace(/"/g, "&quot;")
+              .replace(/'/g, "&apos;");
+}
+
+convertHTML("Dolce & Gabbana");
