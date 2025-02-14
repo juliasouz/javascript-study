31b68fec6db6d2fa3613e6a9609b0d8dfb0b9a22
Julia Souza <julia-souza@uergs.edu.br>
2025-02-14 20:44:55 -0300

    Exercise: Implement steamrollArray function to flatten a nested array

---
+++ freecodecamp/Intermediate Algorithm Scripting/steamroller.js
@@ -0,0 +1,9 @@
+/* Steamroller
+Flatten a nested array. You must account for varying levels of nesting. */
+
+function steamrollArray(arr) {
+    const flat = [].concat(...arr);
+    return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
